c428346fa48ae03acaf02ce98bbbdef1579648b9
vicky raj <vickyraj12004@gmail.com>
2026-02-14 01:20:04 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,11 @@
+import cv2 as cv # to import the oencv module 
+img = cv.imread("img_1") # to read the image from the specified location
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY) #to convert the image into grayscale
+cv.imshow("Window_is_Open",gray) #to show the image 
+h,w,c = img.shape #to find the image size and channels
+print("The details o fthe image is as:\n") 
+print(f"\nHeight: {h}\n Width: {w}\n Channel: {c}\n")
+cv.waitKey(0) #to hold the output on the screen until any key is pressed.
+cv.destroyAllWindows() # to destroy all the open windows on the panel.
+
+
