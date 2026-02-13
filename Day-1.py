21420e90c95e4ac2aadeb859c465856e9a53d965
vicky raj <vickyraj12004@gmail.com>
2026-02-14 01:20:39 +0530

    Fix formatting issues in Day-1.py

--- Day-1.py
+++ Day-1.py
@@ -1,4 +1,4 @@
-import cv2 as cv # to import the oencv module 
+import cv2 as cv # to import the opencv module 
 img = cv.imread("img_1") # to read the image from the specified location
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY) #to convert the image into grayscale
 cv.imshow("Window_is_Open",gray) #to show the image 
@@ -7,5 +7,6 @@ print("The details o fthe image is as:\n")
 print(f"\nHeight: {h}\n Width: {w}\n Channel: {c}\n")
 cv.waitKey(0) #to hold the output on the screen until any key is pressed.
 cv.destroyAllWindows() # to destroy all the open windows on the panel.
+
 
 
